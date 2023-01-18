262f8558087decc27b42cfc73ebbcdfa8a5f4c82
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-18 23:18:33 +0500

    Update menu-herramientas.js

--- plugins/menu-herramientas.js
+++ plugins/menu-herramientas.js
@@ -85,7 +85,7 @@ handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(menuherramientas|\?)$/i
 handler.exp = 50
-handler.register = true
+handler.register = false
 export default handler
 
 function clockString(ms) {
