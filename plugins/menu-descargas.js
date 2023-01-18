1d35e2a039b6051856278af00b082648eaebd13b
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-18 23:22:48 +0500

    Update menu-descargas.js

--- plugins/menu-descargas.js
+++ plugins/menu-descargas.js
@@ -98,7 +98,7 @@ handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(menudescargas|\?)$/i
 handler.exp = 50
-handler.register = true
+handler.register = false
 export default handler
 
 function clockString(ms) {
