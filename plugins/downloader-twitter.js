f105dc437644faf1c3f3a0e4a98396b12499f694
ZidiOFC <122985719+ZidiOFC@users.noreply.github.com>
2023-01-18 19:50:05 +0500

    Ikratos-Hack

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -1,16 +1,16 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚃𝚆𝙸𝚃𝚃𝙴𝚁, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw` 
+if (!text) throw `*ENTRE LINK OF TWITTER POST: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw` 
 let res = await twitterDl(text)
 await m.reply(global.wait)
 for (let x = 0; x < res.media.length; x++) {
-let caption = x === 0 ? res.caption.replace(/https:\/\/t.co\/[a-zA-Z0-9]+/gi, '').trim() : '*𝙰𝚀𝚄𝙸 𝙴𝚂𝚃𝙰 𝚂𝚄 𝚅𝙸𝙳𝙴𝙾*'
+let caption = x === 0 ? res.caption.replace(/https:\/\/t.co\/[a-zA-Z0-9]+/gi, '').trim() : '*HERE IS YOUR VIDEO*'
 await conn.sendFile(m.chat, res.media[x].url, 'error.mp4', caption, m)}}
 handler.command = /^((twdl|tw|twt|twitter)(dl)?)$/i
 export default handler
 async function twitterDl(url) {
 let id = /twitter\.com\/[^/]+\/status\/(\d+)/.exec(url)?.[1]
-if (!id) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝚃𝚆𝙸𝚃𝚃𝙴𝚁, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: #twitter* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw'
+if (!id) throw '*ENTRE VALID LINK OF TWITTER, EXAMPLE: #twitter* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw'
 let res = await fetch(`https://tweetpik.com/api/tweets/${id}`)
 if (res.status !== 200) throw res.statusText
 let json = await res.json()
@@ -25,4 +25,4 @@ media.push({ url: vid.url, type: i.type })
 media.push({ url: i.url, type: i.type })}}
 return {
 caption: json.text, media 
-}} else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*' }
+}} else throw '*404 ERROR*' }
