5a1751171e402204c5c0ddd5254d134a07175e59
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-18 23:02:17 +0500

    Update menu-sticker.js

--- plugins/menu-sticker.js
+++ plugins/menu-sticker.js
@@ -85,7 +85,7 @@ handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(menusticker|\?)$/i
 handler.exp = 50
-handler.register = true
+handler.register = false
 export default handler
 
 function clockString(ms) {
