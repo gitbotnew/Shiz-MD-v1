b4a3503b07203bd1bbfb0d0c3c85e46c554fb8cf
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-18 23:03:14 +0500

    Update menu-serbot.js

--- plugins/menu-serbot.js
+++ plugins/menu-serbot.js
@@ -88,7 +88,7 @@ handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(menuserbot|\?)$/i
 handler.exp = 50
-handler.register = true
+handler.register = false
 export default handler
 
 function clockString(ms) {
