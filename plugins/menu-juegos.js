7d2ecc54e54d88777aab7c132cb6392c3cb3f857
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-18 23:17:39 +0500

    Update menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -101,7 +101,7 @@ handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(menujuegos|\?)$/i
 handler.exp = 50
-handler.register = true
+handler.register = false
 export default handler
 
 function clockString(ms) {
